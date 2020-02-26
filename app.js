168b7791b2cef74fc971ef6c6b2472ea4d5d1879
syazwan <syazwan9889@gmail.com>
2020-02-26 23:11:32 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function renderCafe(doc) {
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
-    let cross = document.createElement('div');
+    let cross = document.createElement('button');
 
     li.setAttribute('data-id', doc.id)
     name.textContent = doc.data().name;
@@ -39,13 +39,14 @@ function renderCafe(doc) {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    db.collection('cafes').add({
-        name: form.name.value,
-        city: form.city.value
-    })
-    form.name.value = '';
-    form.city.value = '';
-
+    if (form.name.value !== "" && form.city.value !== "") {
+        db.collection('cafes').add({
+            name: form.name.value,
+            city: form.city.value
+        })
+        form.name.value = '';
+        form.city.value = '';
+    }
 })
 
 db.collection('cafes').orderBy('name').onSnapshot(snapshot => {
